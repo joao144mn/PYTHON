85f31b3c0c5169387ee73224572ee9b716d8f3b3
João Mazolini <jgseraphinm@gmail.com>
2025-09-25 15:39:34 -0300

    Add files via upload

---
+++ Python.py
@@ -0,0 +1,11 @@
+idade = int(input("Me fale quantos anus tem:"))
+print("vc tem", idade,"anus")
+
+if idade < 1: 
+    print ("c caga por onde ent")
+elif idade == 1:
+    print("eu tbm só tenho um cuzin")
+elif idade > 10:
+    print("valeu o mia khalifa")
+else:
+    print("slk pae ta com bastante buraco")
